838586bb2dfa8992ac265b3947f9d9edc6939fda
mibre2 <michael_breckenridge@ymail.com>
2022-06-02 17:55:13 -0400

    Added Lambda Tutorial

---
+++ 22_XML_&_ElementTree.py
@@ -0,0 +1,9 @@
+# Begin by visiting the documentation here: https://docs.python.org/
+    # Select "Global Module Index"
+    # Scroll down to "xml" and expand it to reveal " xml.etree.ElementTree"
+
+# Open up your shell/terminal and type "import xml.etree.ElementTree as ET"
+import xml.etree.ElementTree as ET
+print(dir(ET))  # This shows a wall of text that isn't easy on the eyes
+from inspect import getmembers, isclass, isfunction
+
