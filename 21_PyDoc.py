4a22b0823d24bed6f0bc657a9c44970ae4f87ead
mibre2 <michael_breckenridge@ymail.com>
2024-04-15 10:22:13 -0400

    commenting before starting

---
+++ 21_PyDoc.py
@@ -0,0 +1,6 @@
+# To look at the pydoc module,
+# go to the terminal and type: "python -m pydoc"
+
+# The first way we'll use pydoc is to show text documentation on a 
+# Keyword, function, or class.
+# Example: python -m pydoc math
